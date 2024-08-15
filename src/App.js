751ce0f68be2392cceed0d11139ce4d0ab1aa6bd
ahsan-kwanso <m.ahsan@kwanso.com>
2024-08-15 14:32:40 +0500

    added globall css file for hiding cursor

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Profile from "./pages/Profile";
 import PostView from "./pages/PostView";
 import Footer from "./components/Footer";
 import { Box } from "@mui/material";
+import "./utils/global.css";
 
 const App = () => {
   return (
