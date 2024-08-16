17a4882e9c7f0dadf4cae35ebb545e8884af013b
ahsan-kwanso <m.ahsan@kwanso.com>
2024-08-16 10:22:47 +0500

    show message when you are redirected to dashboard page when trying to access login

--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useSnackbar } from "../contexts/SnackbarContext";
 
 const PublicRoute = () => {
   const token = localStorage.getItem("token");
+  const { showSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    if (token) {
+      showSnackbar("Already logged in!");
+    }
+  }, [token, showSnackbar]);
 
   if (token) {
     return <Navigate to="/dashboard" />;
