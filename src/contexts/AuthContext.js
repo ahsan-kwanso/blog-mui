4c664888f864d263a2c360dae891b36266f6443d
ahsan-kwanso <m.ahsan@kwanso.com>
2024-08-16 11:37:32 +0500

    refactored auth context, move methods to services and separate file for token operations

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,6 @@
 import React, { createContext, useState, useEffect } from "react";
+import { signup, signin, signout } from "../services/authService";
+import { getToken } from "../utils/authUtils";
 import axiosInstance from "../axiosInstance";
 
 const AuthContext = createContext();
@@ -7,16 +9,13 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   const fetchUser = async () => {
-    const token = localStorage.getItem("token");
+    const token = getToken();
     if (token) {
       try {
         const response = await axiosInstance.get("/users/me");
         setUser(response.data.user);
-        //console.log(response.data.user);
       } catch (error) {
         console.error("Failed to fetch user:", error);
-        // Handle token expiration or invalid token
-        // localStorage.removeItem("token");
         setUser(null);
       }
     }
@@ -26,52 +25,32 @@ const AuthProvider = ({ children }) => {
     fetchUser();
   }, []);
 
-  const signup = async (name, email, password) => {
-    try {
-      const response = await axiosInstance.post("/auth/signup", {
-        name,
-        email,
-        password,
-      });
-      localStorage.setItem("token", response.data.token);
-      await fetchUser();
-      return response;
-    } catch (error) {
-      let errorMessage = "Something went wrong!";
-      if (error.response) {
-        errorMessage = error.response.data.message;
-        if (!error.response.data.message) errorMessage = "Invalid format";
-      }
-      throw new Error(errorMessage);
-    }
+  const handleSignup = async (name, email, password) => {
+    const response = await signup(name, email, password);
+    await fetchUser();
+    return response;
   };
 
-  const signin = async (email, password) => {
-    try {
-      const response = await axiosInstance.post("/auth/signin", {
-        email,
-        password,
-      });
-      localStorage.setItem("token", response.data.token);
-      await fetchUser();
-      return response;
-    } catch (error) {
-      let errorMessage = "Something went wrong!";
-      if (error.response) {
-        errorMessage = error.response.data.message;
-        if (!error.response.data.message) errorMessage = "Invalid format";
-      }
-      throw new Error(errorMessage);
-    }
+  const handleSignin = async (email, password) => {
+    const response = await signin(email, password);
+    await fetchUser();
+    return response;
   };
 
-  const signout = () => {
+  const handleSignout = () => {
+    signout();
     setUser(null);
-    localStorage.removeItem("token");
   };
 
   return (
-    <AuthContext.Provider value={{ user, signup, signin, signout }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        signup: handleSignup,
+        signin: handleSignin,
+        signout: handleSignout,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
